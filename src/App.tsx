edc536aa42f292a824b3e508ad56079c32588020
dijanakalaposratic <dijanakalaposratic@gmail.com>
2024-02-24 01:14:56 +0100

    added app functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,51 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import React, { useState } from 'react';
+import allQuotesData from './assets/allQuotesData.json';
 import './App.css';
 
-function App() {
-	const [count, setCount] = useState(0);
-
-	return (
-		<>
-			<div>
-				<a href='https://vitejs.dev' target='_blank'>
-					<img src={viteLogo} className='logo' alt='Vite logo' />
-				</a>
-				<a href='https://react.dev' target='_blank'>
-					<img src={reactLogo} className='logo react' alt='React logo' />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-		</>
-	);
+// Define the structure of a quote
+interface Quote {
+	text: string;
+	author: string;
 }
 
+// Function to get a random quote from an array of quotes
+const getRandomQuote = (quotesArray: Quote[]): Quote | null => {
+	// Generate a random index
+	const randomIndex = Math.floor(Math.random() * quotesArray.length);
+	// Return the quote at the random index or null if the array is empty
+	return quotesArray[randomIndex] || null;
+};
+
+// Main component
+const App: React.FC = () => {
+	// Use state to keep track of the current quote
+	const [currentQuote, setCurrentQuote] = useState<Quote | null>(getRandomQuote(allQuotesData));
+	// Destructure currentQuote or provide default values
+	const { text, author } = currentQuote || { text: 'No quote available', author: 'Unknown author' };
+	// Function to get a new quote and update the state
+	const getNewQuote = () => {
+		setCurrentQuote(getRandomQuote(allQuotesData));
+	};
+	// Render the quote in a simple HTML structure
+	return (
+		<div id='quote-box'>
+			<div className='quote-content'>
+				<p id='text'>
+					<i className='fa-solid fa-quote-left'></i> {text} <i className='fa-solid fa-quote-right'></i>
+				</p>
+				<p id='author'>- {author}</p>
+				<button>
+					<a className='twitter-share-button' id='tweet-quote' href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(`"${text}" - ${author}`)}`} target='_blank'>
+						Share on X
+					</a>
+				</button>
+				<button id='new-quote' onClick={getNewQuote}>
+					New Quote
+				</button>
+			</div>
+		</div>
+	);
+};
+
+// Export the component as the default export of the file
 export default App;
